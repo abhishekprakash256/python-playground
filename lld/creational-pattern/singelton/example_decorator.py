814e50b8cff03e45c6f4aafd931b73e8a719cded
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-07-24 21:42:37 -0400

    test code

---
+++ lld/creational-pattern/singelton/example_decorator.py
@@ -0,0 +1,27 @@
+"""
+using the decorator 
+"""
+
+def singleton(cls):
+
+    instance_mapper = {}
+
+    def get_instance(*args, **kwargs):
+
+        if cls not in instance_mapper :
+
+            print("The instance has been created")
+
+            instance_mapper[cls] = cls(*args , **kwargs)
+
+        return instance_mapper[cls]
+
+    return get_instance
+
+
+@singleton
+class Logger():
+
+    def log(self,message):
+
+        print()
