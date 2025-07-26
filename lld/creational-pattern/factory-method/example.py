8ef62dfbd3bcb817e86e764c29c9543293a38476
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-07-26 14:11:23 -0400

    test code

---
+++ lld/creational-pattern/factory-method/example.py
@@ -0,0 +1,16 @@
+"""
+
+The factory method is used for the creation
+
+
+
+
+
+links --
+
+https://www.geeksforgeeks.org/system-design/factory-method-for-designing-pattern/#
+
+https://github.com/ArjanCodes/2021-factory-pattern
+
+
+"""
